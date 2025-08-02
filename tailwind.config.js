268a64df248d0df8d63c70e8a1f3bf46a1583d0f
Vidh singh Chauhan <vidha.vsc@gmail.com>
2025-08-02 11:44:43 +0530

    add code and standard colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyber-green': '#00FF41',
+        'cyber-pink': '#FF00D4',
+      },
+    },
   },
   plugins: [],
 };
