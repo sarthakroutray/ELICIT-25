4980b7b447d7dc0604b0c29a0b4de0dd21845cc5
SarthakRoutray <sarthak.routray2006@gmail.com>
2025-08-21 12:16:42 +0530

    Footer Fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,32 +11,23 @@ const Footer = () => {
         {/* Address */}
         <div>
           <h3 className="text-cyan-300 text-xl mb-4 tracking-widest">ELICIT '25</h3>
-          <p className="text-sm leading-loose uppercase">
-            hello@pliskencorp.com<br />
-            17888 67TH COURT NORTH<br />
-            LOXAHATCHEE, FL 33470 US
-          </p>
+          
         </div>
 
         {/* Quick Links */}
         <ul className="text-sm space-y-2 uppercase">
-  <li><a href="#about" className="glitch-hover transition duration-300">About</a></li>
+  <li><a href="/about" className="glitch-hover transition duration-300">About</a></li>
   <li><a href="#blog" className="glitch-hover transition duration-300">Blog</a></li>
-  <li><a href="#faq" className="glitch-hover transition duration-300">FAQ</a></li>
-  <li><a href="#privacy" className="glitch-hover transition duration-300">Privacy Policy</a></li>
-  <li><a href="#login" className="glitch-hover transition duration-300">Login</a></li>
-  <li><a href="#terms" className="glitch-hover transition duration-300">Terms of Use</a></li>
-  <li><a href="#signup" className="glitch-hover transition duration-300">Sign Up</a></li>
 </ul>
 
         {/* Placeholder Column */ }
         <div>
           <h3 className="text-cyan-300 text-xl mb-4 tracking-widest">CONTACT / INFO</h3>
           <ul className="text-sm space-y-2 uppercase">
-            <li><a href="#support" className="glitch-hover transition duration-300">Support</a></li>
-            <li><a href="#sponsors" className="glitch-hover transition duration-300">Sponsorships</a></li>
-            <li><a href="#team" className="glitch-hover transition duration-300">Team</a></li>
-            <li><a href="#updates" className="glitch-hover transition duration-300">Get Updates</a></li>
+            <li><a href="/contact" className="glitch-hover transition duration-300">Support</a></li>
+            <li><a href="/sponsors" className="glitch-hover transition duration-300">Sponsorships</a></li>
+            <li><a href="/team" className="glitch-hover transition duration-300">Team</a></li>
+            <li><a href="/updates" className="glitch-hover transition duration-300">Get Updates</a></li>
           </ul>
         </div>
       </div>
